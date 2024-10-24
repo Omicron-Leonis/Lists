8c7b47619ca8d34e7d2a7aa11922a628f7506fbd
Omicron-Leonis <intisar.mahiyat@gmail.com>
2024-10-24 21:20:49 +0600

    lists

---
+++ Play with lists.py
@@ -0,0 +1,27 @@
+L = [4, 5, 1, 2, 9, 7, 10, 9]
+print("Original List: ", L)
+
+#variabl;e to store the sum of the # list
+# the list
+count = 0
+
+#Finding the sum
+for i in L:
+    count += 1
+
+#divide the total elements by
+#number of elements
+avg = count/len(L)
+
+print("sum =", count)
+print("average =", avg)
+
+#sorting the elements of the list
+L.sort()
+
+#printing the first element
+print("Largest elements is:", L[-1])
+
+#printing the largest element
+print("Largest elements is:", L[-1])
+
